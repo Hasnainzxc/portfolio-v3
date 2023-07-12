23a7002268f2569e37250eb5173e4ae5cb7dad61
hasnainzxc <hasnainzxc@icloud.com>
2023-07-12 10:09:02 -0700

    files for porfolio

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/contact', title: 'Contact' },
+  // { href: '/courses', title: 'Courses' },
+];
+
+export default headerNavLinks;
