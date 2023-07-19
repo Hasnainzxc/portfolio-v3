da23dedecbf185fc5ece3477f9554441eb875e40
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2023-07-19 06:01:11 -0700

    Update ListLayout.tsx

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Header } from '@/components/Form';
 import Link from '@/components/Link';
 import Pagination from '@/components/Pagination';
 import Tag from '@/components/Tag';
 import formatDate from '@/lib/utils/formatDate';
-import { ComponentProps, useState } from 'react';
+import { ComponentProps } from 'react';
 import { BsFilterLeft as FilterIcon } from 'react-icons/bs';
 import { PostFrontMatter } from 'types/PostFrontMatter';
 
@@ -14,80 +16,77 @@ interface Props {
   pagination?: ComponentProps<typeof Pagination>;
 }
 
-export default function ListLayout({
-  posts,
-  title,
-  initialDisplayPosts = [],
-  pagination,
-}: Props) {
-  const [searchValue, setSearchValue] = useState('');
-  const filteredBlogPosts = posts.filter(frontMatter => {
-    const searchContent =
-      frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ');
-    return searchContent.toLowerCase().includes(searchValue.toLowerCase());
-  });
+interface MediumPost {
+  title: string;
+  pubDate: string;
+  link: string;
+  categories: string[];
+  description: string;
+  posts: PostFrontMatter[];
+  
+  initialDisplayPosts?: PostFrontMatter[];
+  pagination?: ComponentProps<typeof Pagination>;
+}
 
-  // If initialDisplayPosts exist, display it if no searchValue is specified
-  const displayPosts =
-    initialDisplayPosts.length > 0 && !searchValue
-      ? initialDisplayPosts
-      : filteredBlogPosts;
+export default function ListLayout({ title, pagination }: Props) {
+  const [posts, setPosts] = useState<MediumPost[]>([]);
+  const [loading, setLoading] = useState(true);
+  
+  
+ 
+  useEffect(() => {
+    // Fetch data from your Medium blog URL
+    axios
+      .get('https://api.rss2json.com/v1/api.json?rss_url=https://hasnainzxc.medium.com/feed')
+      .then(response => {
+        if (response.data.status === 'ok') {
+          setPosts(response.data.items);
+          setLoading(false);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
+
+
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
       <div className='fade-in divide-y-2 divide-gray-100 dark:divide-gray-800'>
         <Header title={title}>
           <div className='relative max-w-lg'>
-            <input
-              aria-label='Search articles'
-              type='text'
-              onChange={({ target }) => setSearchValue(target.value)}
-              placeholder='Search articles'
-              className='block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100'
-            />
-            <svg
-              className='absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-              />
-            </svg>
-            <Link
-              href='/tags'
-              className='absolute right-10 top-2 text-gray-400 dark:text-gray-300'
-            >
-              <FilterIcon size={30} />
-            </Link>
+            {/* ... (existing code) ... */}
+            
           </div>
         </Header>
 
         <ul>
-          {!filteredBlogPosts.length && (
-            <p className='mt-8 text-center'>No posts found</p>
-          )}
-          {displayPosts.map(frontMatter => {
-            const { slug, date, title, summary, tags } = frontMatter;
+          {!posts.length && <p className='mt-8 text-center'>No posts found</p>}
+          {posts.map(post => {
+            const { title, pubDate, link, categories, description } = post;
+            const date = formatDate(pubDate);
+            const tags = categories.map((category: string) => category.trim());
+
             return (
-              <li key={slug} className='py-4'>
+              <li key={link} className='py-4'>
                 <article className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
                   <dl>
                     <dt className='sr-only'>Published on</dt>
                     <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
-                      <time dateTime={date}>{formatDate(date)}</time>
+                      <time dateTime={date}>{date}</time>
                     </dd>
                   </dl>
                   <div className='space-y-3 xl:col-span-3'>
                     <div>
                       <h3 className='text-2xl font-bold leading-8 tracking-tight'>
                         <Link
-                          href={`/blog/${slug}`}
+                          href={link}
                           className='text-gray-900 dark:text-gray-100'
                         >
                           {title}
@@ -99,9 +98,10 @@ export default function ListLayout({
                         ))}
                       </div>
                     </div>
-                    <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
-                      {summary}
-                    </div>
+                    <div
+                      className='prose max-w-none text-gray-500 dark:text-gray-400'
+                      dangerouslySetInnerHTML={{ __html: description }}
+                    />
                   </div>
                 </article>
               </li>
@@ -109,12 +109,7 @@ export default function ListLayout({
           })}
         </ul>
       </div>
-      {pagination && pagination.totalPages > 1 && !searchValue && (
-        <Pagination
-          currentPage={pagination.currentPage}
-          totalPages={pagination.totalPages}
-        />
-      )}
+      {/* ... (existing code) ... */}
     </>
   );
 }
