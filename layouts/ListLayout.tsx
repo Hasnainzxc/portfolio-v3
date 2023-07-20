e5a694fe52365772f4ee9f052e2782bc8192bae2
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2023-07-20 02:56:06 -0700

    Update ListLayout.tsx

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -1,18 +1,17 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Header } from '@/components/Form';
 import Link from '@/components/Link';
 import Pagination from '@/components/Pagination';
 import Tag from '@/components/Tag';
 import formatDate from '@/lib/utils/formatDate';
-import { ComponentProps } from 'react';
+import { ComponentProps, useEffect, useState } from 'react';
 import { BsFilterLeft as FilterIcon } from 'react-icons/bs';
 import { PostFrontMatter } from 'types/PostFrontMatter';
+import axios from 'axios';
+
 
 interface Props {
-  posts: PostFrontMatter[];
   title: string;
-  initialDisplayPosts?: PostFrontMatter[];
+  initialPosts?: PostFrontMatter[];
   pagination?: ComponentProps<typeof Pagination>;
 }
 
@@ -22,22 +21,19 @@ interface MediumPost {
   link: string;
   categories: string[];
   description: string;
-  posts: PostFrontMatter[];
-  
-  initialDisplayPosts?: PostFrontMatter[];
+  initialPosts?: PostFrontMatter[];
   pagination?: ComponentProps<typeof Pagination>;
 }
 
-export default function ListLayout({ title, pagination }: Props) {
+export default function ListLayout({ title, initialPosts = [], pagination }: Props) {
   const [posts, setPosts] = useState<MediumPost[]>([]);
   const [loading, setLoading] = useState(true);
-  
-  
- 
+  const [searchValue, setSearchValue] = useState('');
+
   useEffect(() => {
     // Fetch data from your Medium blog URL
     axios
-      .get('https://api.rss2json.com/v1/api.json?rss_url=https://hasnainzxc.medium.com/feed')
+      .get('https://api.rss2json.com/v1/api.json?rss_url=https://hasnainzxc.medium.com/feed/')
       .then(response => {
         if (response.data.status === 'ok') {
           setPosts(response.data.items);
@@ -50,31 +46,65 @@ export default function ListLayout({ title, pagination }: Props) {
       });
   }, []);
 
-
-
   if (loading) {
     return <p>Loading...</p>;
   }
+
+  const filteredPosts = posts.filter(post => {
+    const searchContent =
+      post.title + post.description + post.categories.join(' ');
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
+  const displayPosts =
+    initialPosts.length > 0 && !searchValue ? initialPosts : filteredPosts;
 
   return (
     <>
       <div className='fade-in divide-y-2 divide-gray-100 dark:divide-gray-800'>
         <Header title={title}>
           <div className='relative max-w-lg'>
-            {/* ... (existing code) ... */}
-            
+            <input
+              aria-label='Search articles'
+              type='text'
+              onChange={({ target }) => setSearchValue(target.value)}
+              placeholder='Search articles'
+              className='block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100'
+            />
+            <svg
+              className='absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+              />
+            </svg>
+            <Link
+              href='/tags'
+              className='absolute right-10 top-2 text-gray-400 dark:text-gray-300'
+            >
+              <FilterIcon size={30} />
+            </Link>
           </div>
         </Header>
 
         <ul>
-          {!posts.length && <p className='mt-8 text-center'>No posts found</p>}
-          {posts.map(post => {
+          {!displayPosts.length && (
+            <p className='mt-8 text-center'>No posts found</p>
+          )}
+          {displayPosts.map((post, index) => {
             const { title, pubDate, link, categories, description } = post;
             const date = formatDate(pubDate);
             const tags = categories.map((category: string) => category.trim());
 
             return (
-              <li key={link} className='py-4'>
+              <li key={index} className='py-4'>
                 <article className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
                   <dl>
                     <dt className='sr-only'>Published on</dt>
@@ -98,10 +128,9 @@ export default function ListLayout({ title, pagination }: Props) {
                         ))}
                       </div>
                     </div>
-                    <div
-                      className='prose max-w-none text-gray-500 dark:text-gray-400'
-                      dangerouslySetInnerHTML={{ __html: description }}
-                    />
+                    <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
+                      {/* <MediumBlog content={description} /> */}
+                    </div>
                   </div>
                 </article>
               </li>
