e26abdde52b7a17ecbf14a66a61365e0724c9dc8
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2024-10-20 06:33:05 -0700

    Update projects.ts

--- config/projects.ts
+++ config/projects.ts
@@ -209,4 +209,28 @@ export const projects: Project[] = [
     },
     subProjects: [],
   },
+  {
+    title: 'Caps Appareal',
+    slug: 'capsappareal',
+    banner: '/static/projects/caps-appareal/hero.png',
+    // banner: '/static/projects/shopsy/banner.png',
+    website: 'https://dadsagree.com/',
+    description: `"Dads Agree" is a unique platform uniting over 30 dedicated fathers from diverse professions, all committed to offering invaluable parenting insights. From developmental milestones to hands-on product reviews, the website serves as a comprehensive guide for parents navigating the complexities of raising children. Each piece of advice and product recommendation is rooted in real-life experiences, ensuring authentic and practical guidance for the parenting community.`,
+    shortDescription:
+      'A platform where dads share expert parenting advice and product reviews.',
+    repository: 'https://github.com/SageTheThird',
+    stack: [
+      Stack.wordpress,
+      Stack.ahreafs,
+      Stack.jira,
+      Stack.slack,
+      Stack.zaplin,
+    ],
+    dimensions: [400, 680],
+    screenshots: ['YCyB9gD.png', 'eTJ1yaL.png', 'BA98Kxb.png', 'lFKpHjV.png',
+    deployment: {
+      web: 'https://dadsagree.com/',
+    },
+    subProjects: [],
+  },
 ];
